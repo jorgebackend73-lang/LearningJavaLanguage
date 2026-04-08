f1c1f9c496de439f308ebb85981e34162defed1c
jorgebackend73-lang <jorgebackend73@gmail.com>
2026-04-09 01:07:53 +0200

    me falta hacer los constructores

--- LearningJavaLanguaje/src/main/java/com/example/Persona.java
+++ LearningJavaLanguaje/src/main/java/com/example/Persona.java
@@ -3,12 +3,32 @@ package com.example;
 public class Persona {
 	
 	/*
-	 * A continuación las variables miembres de la clase: propiedades, campos
+	 * A continuación las variables miembros de la clase: propiedades, campos
 	 * y atributos.
 	 * */
+	
+	/*Modificadores de acceso:
+	 * 
+	 * 1- private del package > es el valor por defecto.
+	 * 2- public > es accesible en todos los paquetes.
+	 * 3- private > solo accesible por los elementos de la misma clase, dentro de
+	 * la misma clase.
+	 * 4- protected > herencia
+	 * 
+	 * Importante una clase bien diseñada tiene todos sus propiedades o campos, 
+	 * privados y solamente se relaciona con el mundo exterior a traves de 
+	 * metodos públicos que acceden a dichas variables privadas.
+	 * */
+	
 	String nombre;
 	String primerApellido;
 	String segundoApellido;
+	
+	/*
+	 * Metodos Getter and Setter > metodos publicos a traves de los cuales se 
+	 * pueden modificar las variables privadas de la clase.
+	 * */
+	
 	
 
 }
