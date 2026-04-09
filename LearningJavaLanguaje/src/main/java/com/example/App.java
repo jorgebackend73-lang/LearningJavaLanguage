9589fd7ef34ce5453ca052dbae5523525d71c42c
jorgebackend73-lang <jorgebackend73@gmail.com>
2026-04-09 11:45:22 +0200

    se ha eliminado de la clase Persona todo el código repetitivo que va a generar lombok

--- LearningJavaLanguaje/src/main/java/com/example/App.java
+++ LearningJavaLanguaje/src/main/java/com/example/App.java
@@ -1,23 +1,8 @@
 package com.example;
 
-/**
- * Hello world!
- */
+
 public class App {
     public static void main(String[] args) {
         
-      Persona persona1 = new Persona();
-      persona1.setNombre("Jorge");
-      System.out.println(persona1.getNombre());
-      
-      var persona3 = new Persona("Duglas", "González", "Villamisar");
-      
-      var persona4 = new Persona("Carolina");
-      
-      var persona5 = new Persona(3500.50);
-      
-      //para imprimir persona3: 
-      
-      System.out.println(persona3.toString());
-    }
+      }
 }
