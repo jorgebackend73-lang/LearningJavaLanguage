361737ef0f210e68d84f87b5271170ec825888db
jorgebackend73-lang <jorgebackend73@gmail.com>
2026-04-09 09:57:43 +0200

    getter and setter puestos

--- LearningJavaLanguaje/src/main/java/com/example/Persona.java
+++ LearningJavaLanguaje/src/main/java/com/example/Persona.java
@@ -6,9 +6,27 @@ public class Persona {
 	 * A continuación las variables miembres de la clase: propiedades, campos
 	 * y atributos.
 	 * */
-	String nombre;
-	String primerApellido;
-	String segundoApellido;
+	private String nombre;
+	private String primerApellido;
+	private String segundoApellido;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
 	
 
 }
