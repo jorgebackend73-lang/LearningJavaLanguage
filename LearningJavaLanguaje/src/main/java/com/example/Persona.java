941dd44459f68fc3eb3de99161cf4afe45abeb2a
jorgebackend73-lang <jorgebackend73@gmail.com>
2026-04-09 14:12:58 +0200

    lombok antes utilizar builder

--- LearningJavaLanguaje/src/main/java/com/example/Persona.java
+++ LearningJavaLanguaje/src/main/java/com/example/Persona.java
@@ -1,11 +1,14 @@
 package com.example;
 
+
+
+@NoArgsConstructor
+@All
 public class Persona {
 		
 	private String nombre;
 	private String primerApellido;
 	private String segundoApellido;
 	private Double salario;
-	
-	
+		
 }
