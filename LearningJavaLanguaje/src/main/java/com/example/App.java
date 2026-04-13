1e5aabf1ac4df036489167b30baeca6f2f4d819f
jorgebackend73-lang <jorgebackend73@gmail.com>
2026-04-13 10:40:05 +0200

    Antes de ver Arrays

--- LearningJavaLanguaje/src/main/java/com/example/App.java
+++ LearningJavaLanguaje/src/main/java/com/example/App.java
@@ -1,58 +1,54 @@
 package com.example;
 
-import java.time.LocalDate;
-import java.time.Month;
-
 public class App {
 	
+	//Declaración variables instancia:
+	private String nombre; //valor por defecto null.
+	private int total; //valor por defecto 0.
+	
 	public static void main(String[] args) {
-        
-    // Solo hemos extendido Empleado a Persona y ya podemos crear instancias
-	// Empleado empleado1 = new Empleado();
-	
-	Empleado empleado1 = Empleado.builder()
-			.build();
-	
-//	Empleado empleado2 = new Empleado(3500.45, Departamento.RRHH, 
-//			LocalDate.of(2000, Month.SEPTEMBER, 1));
-	
-	Empleado empleado2 = Empleado.builder()
-			.salario(3500.45)
-			.departamento(Departamento.RRHH)
-			.fechaAlta(LocalDate.of(2000, Month.SEPTEMBER, 1))
-			.build();
-	
-	System.out.println(empleado2.toString());
-	
 	
 	/*
-	 * Creamos un empleado que tenga todo lo de Persona y lo de Empleado.
-	 * Tener en cuenta que los constructorres no se heredan.
+	 * Variables: 
+	 * 
+	 * https://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.html
+	 * 
+	 * Nombrar variable: primer caracter no pude ser un número. $ y _ se permiten 
+	 * pero tampoco deben usarse como primer caracter de un identificador o nombre 
+	 * de una variable.
+	 * 
+	 * Variables declaradas a nivel de la clase no necesitan tener asignado valor,
+	 * tienen uno asignado por defecto.
+	 * Variables a nivel local (declaradas dentro de un bloque delimitado por llaves
+	 * de apertura y cierre {}) normalmete son declaradas dentro de un metodo.
+	 * 
 	 * */
-    	
-	/* Empleado empleado3 = new Empleado("Juan", "Martinez", "Lopez", Genero.HOMBRE,
-			LocalDate.of(1980, Month.JANUARY, 20), 5000.65, Departamento.FINANZAS,
-			LocalDate.of(2000, Month.SEPTEMBER, 1)); */
-	
-	Empleado empleado3 = Empleado.builder()
-	        
-			// Metodos clase Persona
-			.nombre("Juan")
-	        .primerApellido("Martinez")
-	        .segundoApellido("Lopez")
-	        .genero(Genero.HOMBRE)
-	        .fechaNacimiento(LocalDate.of(1980, Month.JANUARY, 20))
-	        
-	        // Metodos clase Empleado
-	        .salario(5000.65)
-	        .departamento(Departamento.FINANZAS)
-	        .fechaAlta(LocalDate.of(2000, Month.SEPTEMBER, 1))
-	        .ssn("289867544")
-	        .build();
-	
-	System.out.println(empleado3);
-	
-	
-	
-      }
+		
+		int total2 = 0 ;
+		
+	/*
+	 * https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+	 * 
+	 * Tipos de Datos: En Java a la izquierda del nombre de la variable simpre 
+	 * hay que especificar el tipo de dato:
+	 * 
+	 * string, int, double, float.
+	 * 
+	 * Primitivos integrados en el propio lenguaje, los que deberíamos usar
+	 * de forma normal. Identificador todo en minusculas.
+	 * No tienen propiedades ni metodos. Si agregamos el operador . no se va 
+	 * a desplegar ningún menú contextual en Eclipse.
+	 * 
+	 *Objetos o Referencias, envoltorio de un tipo de dato primitivo. Primera letra del
+	 *identificador en mayuscula.
+	 *Pj. Integer envoltorio con metodos y propiedes para mejorar el tipo de dato 
+	 *primitivo. Si usamos operador . nos mostrará menú contextual con esas
+	 *propiedades y metodos agregados al tipo de dato primitivo.
+	 *
+	 * */
+		
+		Integer total3 = 0;
+		
+	}
 }
+
